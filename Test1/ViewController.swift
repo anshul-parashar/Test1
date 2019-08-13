d53afc639dc648a10bde36a4ed36e4140a479c63
anshul <anshul@rajasoftwarelabs.com>
2019-08-13 16:29:33 +0530

    part 2

--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("TEMP: print viewDidLoad 1")
+        print("TEMP: print viewDidLoad 2")
     }
 
 
