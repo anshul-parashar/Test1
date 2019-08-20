5449e06f5cbc5d7495f78e37e0c76f31e2b80acb
anshul <anshul@rajasoftwarelabs.com>
2019-08-20 21:30:58 +0530

    change in master

--- Script/runDangerRules.sh
+++ Script/runDangerRules.sh
@@ -1,10 +1,6 @@
-- script@1.1.2:
-    inputs:
-    - content: |-
-        # fail if any commands fails
-        set -e
-        # debug log
-        set -x
-        bundle install
-        ls
-        bundle exec danger
+# fail if any commands fails
+set -e
+# debug log
+set -x
+bundle install
+bundle exec danger
